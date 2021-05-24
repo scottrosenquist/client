2dfb7c9802ec0c85645b0913ae2a160a927985c2
Scott Rosenquist <scott.rosenquist@mercatus.com>
2021-05-24 13:42:24 -0400

    Add test api call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  console.log(process.env.REACT_APP_API_URL)
+  const [poi, setPoi] = useState([])
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL + '/poi')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          setPoi(result)
+        },
+        (error) => {
+          //setPoi('Error')
+        }
+      )
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +27,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>poi</p>
+        <div>
+          {poi.map((pois, index) => (
+            <div key={index}>{pois.name}</div>
+          ))}
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
